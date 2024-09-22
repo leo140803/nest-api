c27109bcc592aaa147bf3e267b52ae65cf9ea0c6
leonardo <nickholasleonardo@gmail.com>
2024-09-22 16:28:53 +0700

    address service

---
+++ src/address/address.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AddressController } from './address.controller';
+import { AddressService } from './address.service';
+
+@Module({
+  imports: [],
+  controllers: [AddressController],
+  providers: [AddressService],
+  exports: [],
+})
+export class AddressModule {}
